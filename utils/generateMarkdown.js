ce300bef34f727afca2086eb5939aa7c0c1a45d0
Loreina Salalila <vialoraine.gg@gmail.com>
2020-11-17 13:53:00 -0800

    created a function to generate markdown in utils

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ![GitHub](https://img.shields.io/github/license/${data.github}/${data.title})
+  ## Description <a name="description"></a>
+  - ${data.description}
+  ## Table of Contents
+  1. [Description] (#Description)
+  2. [Installation Instructions](#Installation)
+  3. [Usage Information](#usage)
+  4. [License](#license)
+  5. [Contribution Guideline](#contributing)
+  6. [Test Instructions](#test)
+  7. [Questions](#questions)
+  8. [Screenshots](#screenshots)
+ 
+ 
+  ### Installation Instructions <a name="installation"></a>
+  - ${data.installation}
+  ### Usage Information <a name="Usage"></a>
+  - ${data.usage}
+  ## License <a name="license"></a>
+  - This application is covered under ${data.license} license. 
+  ## Contribution Guideline <a name="contribution"></a>
+  - ${data.contribution}
+  ## Test Instructions <a name="tests"></a>
+  - ${data.testing}
+  ## Questions <a name="questions"></a>
+  Any questions? Please Contact Below
+  - GitHub Link: 
+  [${data.GitHub}](https://github.com/${data.github}) 
+  - Email: 
+  ${data.email}
+  ## Screenshots <a name="screenshots"></a>
+  ![](././Develop/screenshots/Screenshot1.png)
+  
 `;
 }
 
